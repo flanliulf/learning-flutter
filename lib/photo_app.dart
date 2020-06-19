7759c14249fa95d18b348d217d8e54528db4435c
liufan <flanliulf@gmail.com>
2020-06-19 09:31:47 +0800

    使用 ListTile 实现拍照和从相册选择两种方式添加图片

--- lib/photo_app.dart
+++ lib/photo_app.dart
@@ -13,8 +13,11 @@ class _PhotoAppState extends State<PhotoApp> {
   File _image;
   final picker = ImagePicker();
 
-  Future getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.camera);
+  Future getImage(bool isTakePhoto) async {
+    Navigator.pop(context);
+
+    final pickedFile = await picker.getImage(
+        source: isTakePhoto ? ImageSource.camera : ImageSource.gallery);
 
     setState(() {
       _image = File(pickedFile.path);
@@ -35,17 +38,37 @@ class _PhotoAppState extends State<PhotoApp> {
         ),
       ),
       body: Center(
-        child: _image == null
-            ? Text('No image selected.')
-            : Image.file(_image),
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        tooltip: 'Pick Image',
+        onPressed: _pickImage,
+        tooltip: '选择图片',
         child: Icon(Icons.add_a_photo),
       ),
     );
   }
 
+  void _pickImage() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              height: 160,
+              child: Column(
+                children: <Widget>[
+                  _item('拍照', true),
+                  _item('从相册选择', false),
+                ],
+              ),
+            ));
+  }
 
+  _item(String title, bool isTakePhoto) {
+    return GestureDetector(
+      child: ListTile(
+        leading: Icon(isTakePhoto ? Icons.camera_alt : Icons.photo_library),
+        title: Text(title),
+        onTap: () => getImage(isTakePhoto),
+      ),
+    );
+  }
 }
